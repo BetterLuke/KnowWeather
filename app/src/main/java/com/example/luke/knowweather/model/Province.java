83c8a7ad082cdef09172ef90ee4d9ea6d267862c
LUKE <1147357044@qq.com>
2015-09-12 16:36:43 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ app/src/main/java/com/example/luke/knowweather/model/Province.java
@@ -0,0 +1,36 @@
+package com.example.luke.knowweather.model;
+
+/**
+ * Province表对应的实体类
+ * Created by LUKE on 2015/9/12.
+ */
+public class Province {
+
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
